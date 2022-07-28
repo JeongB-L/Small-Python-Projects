7ca074ee5e513779badc5fb2c56afe3057b1057f
JeongB-L <70557130+JeongB-L@users.noreply.github.com>
2022-07-29 01:22:12 +0900

    Add files via upload

---
+++ src/multiplication_of_the_rest.py
@@ -0,0 +1,69 @@
+#   given a list, return the results of all multiplications for all the numbers except the current output[i]
+#   WARNING : do not use division for solving this problem
+
+def my_own_solution1(nums: list[int]):
+    #   assuming there is no duplicate
+    temp = []
+    mul = 1
+    result = []
+    for i in range(len(nums)):
+        crr = nums[i]
+        for j in range(len(nums)):
+            if nums[j] != crr:
+                temp.append(nums[j])
+        for element in temp:
+            mul *= element
+        result.append(mul)
+        #   reset the temporarily used variables
+        mul = 1
+        temp = []
+    return result
+
+def my_own_solution_with_no_assumption(nums: list[int]):
+    #   the only difference is that I must distinguish whether it is at the current index by index
+    mul = 1
+    result = []
+    temp = []
+    length = len(nums)
+    for i in range(length):
+        for j in range(length):
+            if j != i:
+                temp.append(nums[j])
+        for element in temp:
+            mul *= element
+        result.append(mul)
+        mul = 1
+        temp = []
+    return result
+
+def multiply_left_and_right(nums: list[int]) -> list[int]:
+    #   multiply the result of the multiplication from left and right except myself
+    #   the multiplication of every number except output[i] means each element is not added by once
+    #   1 -> 1 -> 2 -> 6
+    #   24   12   4   1
+    out = []
+    crr = 1
+    for i in range(len(nums)):
+        out.append(crr)
+        crr *= nums[i]
+    crr = 1
+    #   (len(nums) - 1, -1, -1) means it goes to the opposite direction
+    temp = []
+    for i in range(len(nums) - 1, -1, -1):
+        #   multiplied ones from the right side are multiplied to the ones from the left side
+        out[i] *= crr
+        crr *= nums[i]
+        #   understood the logic, but still don't know why the multiplication of left and right become to the answer
+    return out
+
+def test(input):
+    temp = []
+    #   the closing bound is -1 because if 0 then it won't include the 0th index
+    for i in range(len(input) - 1, -1, -1):
+        temp.append(i)
+    return temp
+if '__main__' == __name__:
+    input = [1, 2, 3, 4]
+    print(my_own_solution1(input))
+    print(my_own_solution_with_no_assumption(input))
+    print(multiply_left_and_right(input))
