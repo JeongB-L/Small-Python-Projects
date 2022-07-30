88d2ca9dff31afb61135a6a9a88ef713cd1d3d66
JeongB-L <70557130+JeongB-L@users.noreply.github.com>
2022-07-30 21:32:05 +0900

    Update stock_trading.py
    added another solution

--- src/stock_trading.py
+++ src/stock_trading.py
@@ -1,5 +1,9 @@
 #   return the biggest profit possible by a single trading
 #   each element is the point in the graph. if you buy and sell at 1, your profit is -6
+
+import sys
+
+
 def my_own_solution(prices: list[int]) -> int:
     length = len(prices)
     prices.reverse()
@@ -18,11 +22,22 @@ def brute_force(prices: list[int]) -> int:
             price = max(prices[j] - crr, price)
     return price
 
+def OtotheN_solution(prices: list[int]) -> int:
+    minimum = sys.maxsize
+    profit = 0
+    #   comparing the previous minimums.
+    #   Get the smallest value and keep track of the smallest
+    #   and then calculate the maximum profit with it
+    #   repeat it until it's over
+    for price in prices:
+        minimum = min(minimum, price)
+        profit = max(profit, price - minimum)
+    return profit
 
 if '__main__' == __name__:
     input = [7, 1, 5, 3, 6, 4]
     print(brute_force(input))
     print(my_own_solution(input))
     input.reverse()
-    
-    print(input)
+    print(OtotheN_solution(input))
+
